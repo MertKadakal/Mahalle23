86637fae407ba5bb9be410ac634ca5a8ecece841
MertKadakal <mert.kadakal1629@gmail.com>
2024-09-25 00:48:44 +0300

    Hava Durumu ekranı büyük oranda tamamlandı

--- app/src/main/java/mert/kadakal/myapplication/HavaDurumu.java
+++ app/src/main/java/mert/kadakal/myapplication/HavaDurumu.java
@@ -3,24 +3,32 @@ package mert.kadakal.myapplication;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.text.Html;
-import android.widget.ImageView;
+import android.text.Spanned;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.bumptech.glide.Glide;
-
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class HavaDurumu extends AppCompatActivity {
 
     TextView hava_durumu_bilgiler;
     TextView anlik_derece;
     TextView yukleniyor;
-    ImageView suanki_hava_gorsel;
+    TextView hissedilen;
+
+    TextView sonrakigun1;
+    TextView sonrakigun2;
+    TextView sonrakigun3;
+    TextView sonrakigun4;
+    TextView sonrakigun5;
+    TextView sonrakigun6;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,12 +36,19 @@ public class HavaDurumu extends AppCompatActivity {
         setContentView(R.layout.hava_durumu);
 
         hava_durumu_bilgiler = findViewById(R.id.hava_durumu_bilgiler);
-        suanki_hava_gorsel = findViewById(R.id.suanki_hava_gorsel);
         anlik_derece = findViewById(R.id.anlik_derece);
         yukleniyor = findViewById(R.id.yukleniyor);
+        hissedilen = findViewById(R.id.hissedilen);
+
+        sonrakigun1 = findViewById(R.id.sonrakigun1);
+        sonrakigun2 = findViewById(R.id.sonrakigun2);
+        sonrakigun3 = findViewById(R.id.sonrakigun3);
+        sonrakigun4 = findViewById(R.id.sonrakigun4);
+        sonrakigun5 = findViewById(R.id.sonrakigun5);
+        sonrakigun6 = findViewById(R.id.sonrakigun6);
 
         // Trigger AsyncTask to fetch news data in the background
-        new HavaDurumu.FetchNewsTask().execute("https://www.google.com/search?q=23+nisan+mahallesi+bursa+hava+durumu&sca_esv=414fa47fd96e69ee&sca_upv=1&rlz=1C1NDCM_enTR1036TR1036&sxsrf=ADLYWIKkgG-8YcYmASlCuYex6yJwt1LlGA%3A1727115306533&ei=KrDxZoGhIPGQxc8Ph8qy2Qw&ved=0ahUKEwiB3tPy1dmIAxVxSPEDHQelLMsQ4dUDCA8&uact=5&oq=23+nisan+mahallesi+bursa+hava+durumu&gs_lp=Egxnd3Mtd2l6LXNlcnAiJDIzIG5pc2FuIG1haGFsbGVzaSBidXJzYSBoYXZhIGR1cnVtdTIKECEYoAEYwwQYCjIKECEYoAEYwwQYCkiVDFDZBVinC3ABeAGQAQCYAcMBoAG5BqoBAzAuNbgBA8gBAPgBAZgCA6ACswLCAgoQABiwAxjWBBhHmAMAiAYBkAYIkgcDMS4yoAeUHQ&sclient=gws-wiz-serp");
+        new HavaDurumu.FetchNewsTask().execute("https://havadurumu15gunluk.xyz/havadurumu/293/bursa-hava-durumu-7-gunluk.html");
     }
 
     private class FetchNewsTask extends AsyncTask<String, Void, Document> {
@@ -54,23 +69,48 @@ public class HavaDurumu extends AppCompatActivity {
         protected void onPostExecute(Document doc) {
             super.onPostExecute(doc);
 
-            String suanki_derece = doc.getElementById("wob_tm").text();
-            String yagis = doc.getElementById("wob_pp").text();
-            String nem = doc.getElementById("wob_hm").text();
-            String ruzgar = doc.getElementById("wob_ws").text();
-            String tarih = doc.getElementById("wob_dts").text();
-            String suanki_hava_png = doc.getElementById("wob_tci").attr("src");
-
-            hava_durumu_bilgiler.setText(Html.fromHtml(String.format("<b>%s</b><br><br>Yağış: %s<br>Nem: %s<br>Rüzgar: %s<br>", tarih, yagis, nem, ruzgar)));
-            anlik_derece.setText(suanki_derece + " °C");
-            runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    // Glide kullanarak görseli ImageView'a yükle
-                    Glide.with(HavaDurumu.this).load("https:" + suanki_hava_png).into(suanki_hava_gorsel);
+            Elements liElements = doc.select("ul.aktuel > li");
+            ArrayList<String> lielements = new ArrayList<>();
+            for (Element li : liElements) {
+                String label = li.textNodes().get(0).text().trim(); // Etiket ismini alır
+                String value = li.select("span.bold").text().trim(); // Span içindeki değeri alır
+                if (!(label.equals("Görüş Uzaklığı")) && !(label.equals("Çiy Noktası")) && !(label.equals("Bulut Oranı")) && !(label.equals("Basınç")) && !(label.equals("Rakım"))) {
+                    lielements.add(value);
                 }
-            });
+            }
 
+            Elements trElements = doc.select("div.weather-forecast > table > tbody > tr");
+            sonrakigun1.setText(Html.fromHtml("<b>"+trElements.get(2).text().replace("Saatlik ", "").substring(0, trElements.get(2).text().replace("Saatlik ", "").indexOf(" ")) + "</b><br>" + trElements.get(2).text().replace("Saatlik ", "").substring(trElements.get(2).text().replace("Saatlik ", "").indexOf(" ") + 1)));
+            sonrakigun2.setText(replaceThirdSpaceWithNewline(trElements.get(3).text().replace("Saatlik ", "")));
+            sonrakigun3.setText(replaceThirdSpaceWithNewline(trElements.get(4).text()));
+            sonrakigun4.setText(replaceThirdSpaceWithNewline(trElements.get(5).text()));
+            sonrakigun5.setText(replaceThirdSpaceWithNewline(trElements.get(6).text()));
+            sonrakigun6.setText(replaceThirdSpaceWithNewline(trElements.get(7).text()));
+
+
+            String suanki_derece = doc.selectFirst("span.temperature.type-1").text();
+            String hissedilen_derece = doc.selectFirst("span.temperature.type-2").text();
+            String ruzgar = lielements.get(0);
+            String nem = lielements.get(1);
+            String gun_d = lielements.get(2);
+            String gun_b = lielements.get(3);
+
+            hava_durumu_bilgiler.setText(Html.fromHtml(String.format("<b>Nem: </b>%s<br><b>Rüzgar: </b>%s<br><b>Gün Doğumu: </b>%s<br><b>Gün Batımı: </b>%s", nem, ruzgar, gun_d, gun_b)));
+            anlik_derece.setText(suanki_derece);
+            hissedilen.setText(hissedilen_derece);
+
+        }
+
+        private Spanned replaceThirdSpaceWithNewline(String input) {
+            int firstIndex = input.indexOf(" ");
+            int secondIndex = input.indexOf(" ", firstIndex + 1);
+            int thirdIndex = input.indexOf(" ", secondIndex + 1);
+
+            if (thirdIndex != -1) {
+                return Html.fromHtml(String.format("<b>%s</b><br>%s", input.substring(0, thirdIndex), input.substring(thirdIndex + 1)));
+                //return input.substring(0, thirdIndex) + "\n" + input.substring(thirdIndex + 1);
+            }
+            return Html.fromHtml(input);  // Üçüncü boşluk yoksa orijinal stringi döndür
         }
     }
 }
