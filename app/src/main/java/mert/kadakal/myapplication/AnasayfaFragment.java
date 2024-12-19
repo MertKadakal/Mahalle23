dcfda92d03b31d6704188daf79549276bd5984e4
MertKadakal <mert.kadakal1629@gmail.com>
2024-12-20 00:40:37 +0300

    comit

--- app/src/main/java/mert/kadakal/myapplication/AnasayfaFragment.java
+++ app/src/main/java/mert/kadakal/myapplication/AnasayfaFragment.java
@@ -7,12 +7,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
+import com.bumptech.glide.Glide;
 
 public class AnasayfaFragment extends Fragment implements View.OnClickListener {
 
